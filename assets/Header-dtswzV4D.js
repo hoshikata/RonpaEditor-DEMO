ed42e431447432fab293a26396395fb955873cc9
hoshikata <os88430@gmail.com>
2024-07-12 22:44:16 +0800

    20240709 preview menu

--- assets/Header-dtswzV4D.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,o,c as t,x as c,a as l,n as a}from"./index-BG_E9A1d.js";const n={__name:"Header",props:{col:{type:[String],default:""}},setup(e){return(r,d)=>(o(),t("header",{class:a(["setting_header",e.col])},[c(r.$slots,"default"),l("hr",{class:a(["rounded-full border-none bg-current h-[5px]",e.col])},null,2)],2))}},_=s(n,[["__scopeId","data-v-2965f9f4"]]);export{_ as H};
