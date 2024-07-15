66c6508765cee26b2c0061cf70f7d0e599582e1a
hoshikata <os88430@gmail.com>
2024-07-15 23:39:20 +0800

    20240715 update

--- assets/Header-BHSzvAIR.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,o,c as t,e6 as c,a as l,n as a}from"./index-CjZ2OxDy.js";const n={__name:"Header",props:{col:{type:[String],default:""}},setup(e){return(r,d)=>(o(),t("header",{class:a(["setting_header",e.col])},[c(r.$slots,"default"),l("hr",{class:a(["rounded-full border-none bg-current h-[5px]",e.col])},null,2)],2))}},_=s(n,[["__scopeId","data-v-2965f9f4"]]);export{_ as H};
